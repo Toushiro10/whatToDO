cefb0596d44fac975e23f7aab35aa23699f9fc9c
zed-chi <zed-chi@mail.ru>
2021-06-22 16:03:15 +0300

    убрал лишние циклы и переменные

--- java.js
+++ java.js
@@ -1,73 +1,62 @@
-var button = document.getElementById("enter"); 
+var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector('ul');
-var close = document.getElementsByClassName("close")
-var existedLi = document.getElementsByTagName("li");
-var close = document.getElementsByClassName("close")
-var li1 = document.querySelectorAll("li");
-var i;
-
-
-//create delete button to existed list
-for (i = 0; i < existedLi.length; i++) {
-  var delBut = document.createElement("button");
-  delBut.innerText = "\u00D7";
-  delBut.className = "close";
-  existedLi[i].appendChild(delBut);
-  delBut.addEventListener("click", function() {
-			i.remove()});
- }
-
-//click on the delete button of existd list
-for(i=0; i<close.length; i++) {
-	close[i].onclick = function() {
-		var div = this.parentElement;
-		div.style.display = "none";
-			}
-	}
-
-//toggle the existed list line
-li1.forEach(function (li) {
-	li.addEventListener("click", function(){
-	li.classList.toggle("done");
-	})
-})
-
+var ul = document.querySelector("ul");
 
 //function to create new element after input + create a del button + make a toggle for the created element
-function createListElement() {
-	var li = document.createElement("li");
-	var btn = document.createElement("button");
-		btn.innerText = "\u00D7";		
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		li.appendChild(btn);
-		btn.className = "close";
-		btn.addEventListener("click", function() {
-			li.remove()});
-		li.addEventListener("click", function(){
-		li.classList.toggle("done");})
-		input.value = "";
+function createListElement(textContent) {
+    var li = document.createElement("li");
+    var deleteButton = document.createElement("button");
+
+    deleteButton.className = "close";
+    deleteButton.innerText = "\u00D7";
+    deleteButton.addEventListener("click", function () {
+        li.remove();
+    });
+
+    li.appendChild(document.createTextNode(textContent));
+    li.appendChild(deleteButton);
+    li.addEventListener("click", function () {
+        this.classList.toggle("done");
+    });
+
+    ul.appendChild(li);
 }
 
 function inputLength() {
-	return input.value.length;
+    return input.value.length;
 }
 
-
 function addToListAfterClick() {
-	if (inputLength()>0) {
-		createListElement()
-	}
+    if (inputLength() > 0) {
+        createListElement(input.value);
+        input.value = "";
+    }
 }
 
 function addToListAfterEnter(event) {
-	if (inputLength()>0 && event.keyCode === 13) {
-		createListElement()
-	}
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement(input.value);
+        input.value = "";
+    }
 }
 
+function addExampleListElements() {
+    var example_tasks = [
+        "Bread",
+        "Milk",
+        "Sugar",
+        "Potato",
+        "Cabbage",
+        "Chocolate",
+    ];
+    for (var task of example_tasks) {
+        createListElement(task);
+    }
+}
 
 button.addEventListener("click", addToListAfterClick);
 input.addEventListener("keypress", addToListAfterEnter);
 
+
+/* adding example tasks*/
+window.onload = addExampleListElements();
